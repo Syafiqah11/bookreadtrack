5c9c262042491adf7c392cf90102ac0092abdab4
Ikaa <nurul.syafiqah@besquare.com.my>
2022-07-22 14:47:23 +0800

    delete

--- src/Components/MyList.js
+++ src/Components/MyList.js
@@ -81,9 +81,7 @@ export default class MyList extends React.Component {
           </div>
 
           <div className="mylist-container">
-            <div>
-              <BookCrud />
-            </div>
+            <div>{/* <BookCrud /> */}</div>
           </div>
 
           {/* <div className="mylist-container">
